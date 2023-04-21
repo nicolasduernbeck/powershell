8829052a0c737bcf53d47bebd0ea523123cd20de
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2023-04-21 18:31:03 +0200

    Learned about common parameters

--- common-paramter.ps1
+++ common-paramter.ps1
@@ -1,3 +1,14 @@
 # -WhatIf
 # -Confirm
 # -Verbose
+
+# -WhatIf, zeigt an was das ausführen des cmdlets tun würde
+Restart-Computer -WhatIf
+
+# -Confirm, fordert zur bestätigung auf
+
+Restart-Computer -Confirm
+
+# -Verbose, zeigt an was PowerShell tun würde
+
+Restart-Computer -Verbose
