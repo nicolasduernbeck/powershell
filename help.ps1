dccb3e91a1b129d37cb070f67ac8bcdf2ea6edc6
= <=>
2023-04-24 11:51:49 +0200

    Learned about Get-Help

---
+++ help.ps1
@@ -0,0 +1,5 @@
+#Zeigt Hilfe für ein cmdlet an (mit dem -Online parameter wird die Hilfe im Browser geöffnet)
+Get-Help New-Alias -Online
+
+#Updated die Hilfe
+Update-Help -UICulture 'en-US' -Force
